1da50dcd8d8f1930e7567dfccc8a5ccff417ac29
Dreh3 <andressa_sousaf@outlook.com>
2025-07-22 08:29:29 -0300

    adicionando primeira página

--- WeatherStation.c
+++ WeatherStation.c
@@ -16,8 +16,8 @@
 
 
 //Crendenciais de wi-fi
-#define WIFI_SSID "TEMPLATE"
-#define WIFI_PASS "TEMPLATE"
+#define WIFI_SSID "Lima"
+#define WIFI_PASS "Harrypotter"
 //----------------------------------------------------
 
 //definindo pinos 
@@ -38,25 +38,132 @@ PIO pio = pio0;
 uint sm = 0;  
 
 //variáveis importantes
-float temperatura_final = 0;  //médias das duas temperaturas lidas
+float temperatura_final = 30;  //médias das duas temperaturas lidas
 int32_t pressao = 0;
 float umidade = 0;
 float max_umidade = 0;
 float min_umidade = 0;
 float max_temperatura = 0;
 float min_temperatura = 0;
-float offset_pressão = 0;
-float offset_temperatura = 0;
+float min_pressao = 0;
+float max_pressao = 0;
+float offset_pressao = 0;
+float offset_umidade = 0;
+float offset_temperatura = 0;  //falta máximo e mínimo e offset
 
 
 //Configurações para a interface WEB --------------------------------------------------
 bool wifi_connection = false;  //armazena status do wi-fi
 const char HTML_BODY[] = //página formatada
-    "";
+    "<!DOCTYPE html>\r\n"
+    "<html lang=\"pt-BR\">\r\n"
+    "<head>"
+    "<meta charset=\"UTF-8\" />"
+     "<title>Interface WEB</title>"
+      "<style>"
+        "body { font-family: sans-serif; background: #f0f2f5; margin: 0; padding: 20px; }"
+        ".container { max-width: 500px; margin: auto; background: white; padding: 20px;"
+            "border-radius: 12px; box-shadow: 0 2px 10px rgba(0,0,0,0.1); }"
+        "h1, h2 { text-align: center; color: #292929; }"
+        ".sensor { display: flex; justify-content: space-between; margin: 10px 0; font-size: 1.1em; }"
+        "form label { font-size: 0.9em; margin-top: 10px; display: block; }"
+        "form input { width: 100%; padding: 6px; font-size: 1em; margin-bottom: 10px;"
+        "border: 1px solid #ccc; border-radius: 6px; }"
+        "button { width: 100%; background: #4CAF50; color: white; padding: 10px; font-size: 1em;"
+        "border: none; border-radius: 8px; cursor: pointer; }"
+        "button:hover { background: #43a047; }"
+        ".titulo-secao { margin-top: 30px; font-size: 1.1em; color: #555; text-align: center;"
+        "border-top: 1px solid #ddd; padding-top: 15px; }"
+      "</style>"
+      "<script>"
+       "window.onload = function () {"
+        "function atualizar() {"
+        "fetch('/dados').then(res => res.json()).then(d => {"
+        "document.getElementById('temp').innerText =  d.temp != null ? d.temp.toFixed(2) : \"--\";"
+        "document.getElementById('hum').innerText = d.hum != null ? d.hum.toFixed(2) : \"--\";"
+        "document.getElementById('press').innerText = d.press != null ? d.press.toFixed(2) : \"--\";"
+        "document.getElementById('a_tempMin').innerText = d.tempMin != null ? d.tempMin.toFixed(1) : \"--\";"
+        "document.getElementById('a_tempMax').innerText = d.tempMax != null ? d.tempMax.toFixed(2) : \"--\";"
+        "document.getElementById('a_tempOffset').innerText = d.tempOffset != null ? d.tempOffset.toFixed(1) : \"--\";"
+        "document.getElementById('a_humMin').innerText = d.humMin != null ? d.humMin.toFixed(1) : \"--\";"
+        "document.getElementById('a_humMax').innerText = d.humMax != null ? d.humMax.toFixed(1) : \"--\";"
+        "document.getElementById('a_humOffset').innerText = d.humOffset != null ? d.humOffset.toFixed(1) : \"--\";"
+        "document.getElementById('a_pressMin').innerText = d.pressMin != null ? d.pressMin.toFixed(1) : \"--\";"
+        "document.getElementById('a_pressMax').innerText = d.pressMax != null ? d.pressMax?.toFixed(1) : \"--\";"
+        "document.getElementById('a_pressOffset').innerText = d.pressOffset != null ? d.pressOffset?.toFixed(1) : \"--\";"
+        "});"
+        "}"
+        "setInterval(atualizar, 1000);"
+        "};"
+
+        "function enviarConfig(event) {"
+        "event.preventDefault();"
+        "const form = event.target;"
+        "const dados = {"
+        "tempMin: parseFloat(form.tempMin.value),"
+        "tempMax: parseFloat(form.tempMax.value),"
+        "tempOffset: parseFloat(form.tempOffset.value),"
+        "humMin: parseFloat(form.humMin.value),"
+        "humMax: parseFloat(form.humMax.value),"
+        "humOffset: parseFloat(form.humOffset.value),"
+        "pressMin: parseFloat(form.pressMin.value),"
+        "pressMax: parseFloat(form.pressMax.value),"
+        "pressOffset: parseFloat(form.pressOffset.value)"
+        "};"
+        "fetch('/config', {"
+        "method: 'POST',"
+        "headers: {"
+        "'Content-Type': 'application/json'"
+        "},\n"
+        "body: JSON.stringify(dados)"
+        "})"
+        ".catch(err => console.error(err));"
+    "}"
+
+        "</script>"
+
+    "</head>"
+    "<body>"
+        "<div class=\"container\">"
+        "<h1>Dados dos Sensores</h1>"
+        "<div class=\"sensor\"> Temperatura: <span id=\"temp\">--</span> °C</div>"
+        "<div class=\"sensor\"> Umidade: <span id=\"hum\">--</span> %</div>"
+        "<div class=\"sensor\"> Pressão: <span id=\"press\">--</span> kPa</div>"
+        "<div class=\"titulo-secao\">Limites e Calibração</div>"
+        "<form onsubmit=\"enviarConfig(event)\">"
+        "<label>Temp Min (Valor atual: <span id=\"a_tempMin\">--</span> °C)"
+        "<input name=\"tempMin\" type=\"number\" step=\"0.1\"></label>"
+        "<label>Temp Max (Valor atual: <span id=\"a_tempMax\">--</span> °C)"
+            "<input name=\"tempMax\" type=\"number\" step=\"0.1\"></label>"
+        "<label>Offset Temp (Valor atual: <span id=\"a_tempOffset\">--</span>)"
+        "<input name=\"tempOffset\" type=\"number\" step=\"0.1\"></label>"
+        "<label>Umid Min (Valor atual: <span id=\"a_humMin\">--</span> %)"
+        "<input name=\"humMin\" type=\"number\" step=\"0.1\"></label>"
+        "<label>Umid Max (Valor atual: <span id=\"a_humMax\">--</span> %)"
+        "<input name=\"humMax\" type=\"number\" step=\"0.1\"></label>"
+        "<label>Offset Umid (Valor atual: <span id=\"a_humOffset\">--</span>)"
+        "<input name=\"humOffset\" type=\"number\" step=\"0.1\"></label>"
+        "<label>Press Min (Valor atual: <span id=\"a_pressMin\">--</span> kPa)"
+        "<input name=\"pressMin\" type=\"number\" step=\"0.1\"></label>"
+        "<label>Press Max (Valor atual: <span id=\"a_pressMax\">--</span> kPa)"
+        "<input name=\"pressMax\" type=\"number\" step=\"0.1\"></label>"
+        "<label>Offset Pressão (Valor atual: <span id=\"a_pressOffset\">--</span>)"
+            "<input name=\"pressOffset\" type=\"number\" step=\"0.1\"></label>"
+        "<button type=\"submit\">Salvar</button>"
+        "</form>"
+        "<div class=\"titulo-secao\">Visualização</div>"
+        "<div style=\"text-align: center; margin-top: 10px;\">"
+        "<a href=\"graficos\" style=\"text-decoration: none; color: #4CAF50; font-weight: bold;\">"
+        "Ver Gráficos</a>"
+        "</div>"
+      "</div>"
+    "</body>"
+    "</html>";
+
 
 struct http_state
 {
-    char response[4096];
+    char response[4700];
     size_t len;
     size_t sent;
 };
@@ -92,6 +199,82 @@ static err_t http_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t er
     hs->sent = 0;
 
     //ações do código
+    if (strstr(req, "GET /dados"))
+    {
+        
+        char json_payload[300];
+        int json_len = snprintf(json_payload, sizeof(json_payload),
+                                "{\"temp\":%.2f,\"hum\":%.2f,\"press\":%d,\"tempMin\":%.2f,\"tempMax\":%.2f,\"tempOffset\":%.2f,\"humMin\":%.2f,\"humMax\":%.2f,\"humOffset\":%.2f,\"pressMin\":%.2f,\"pressMax\":%.2f,\"pressOffset\":%.2f}\r\n",
+                                temperatura_final,umidade, pressao, min_temperatura, max_temperatura, offset_temperatura, min_umidade, max_umidade, offset_umidade, min_pressao, max_pressao, offset_pressao);
+
+        printf("[DEBUG] JSON: %s\n", json_payload);
+
+        hs->len = snprintf(hs->response, sizeof(hs->response),
+                           "HTTP/1.1 200 OK\r\n"
+                           "Content-Type: application/json\r\n"
+                           "Content-Length: %d\r\n"
+                           "Connection: close\r\n"
+                           "\r\n"
+                           "%s",
+                           json_len, json_payload);
+    }
+   else if (strstr(req, "POST /config")) {
+
+    printf("CHAMA O POST");
+    char *read_data = strstr(req, "\r\n\r\n");
+    if (read_data) {
+        read_data += 4;
+        int lidos = sscanf(read_data,
+            "{\"tempMin\":%f,\"tempMax\":%f,\"tempOffset\":%f,"
+            "\"humMin\":%f,\"humMax\":%f,\"humOffset\":%f,"
+            "\"pressMin\":%f,\"pressMax\":%f,\"pressOffset\":%f}",
+            &min_temperatura, &max_temperatura, &offset_temperatura,
+            &min_umidade, &max_umidade, &offset_umidade,
+            &min_pressao, &max_pressao, &offset_pressao);
+
+        if (lidos == 9) {
+            printf(" POST recebido com sucesso:\n");
+            printf("Temp: min=%.1f, max=%.1f, offset=%.1f\n", min_temperatura, max_temperatura, offset_temperatura);
+            printf("Umid: min=%.1f, max=%.1f, offset=%.1f\n", min_umidade, max_umidade, offset_umidade);
+            printf("Press: min=%.1f, max=%.1f, offset=%,1f\n", min_pressao, max_pressao, offset_pressao);
+        } else {
+            printf(" Erro ao interpretar JSON no POST! Campos reconhecidos: %d\n", lidos);
+            printf("Conteúdo recebido: %s\n", read_data);
+        }
+
+    }
+
+    // Cria resposta
+    char json_payload[200];
+    int json_len = snprintf(json_payload, sizeof(json_payload),
+        "{\"tempMin\":%.2f,\"tempMax\":%.2f,\"tempOffset\":%.2f,"
+        "\"humMin\":%.2f,\"humMax\":%.2f,\"humOffset\":%.2f,"
+        "\"pressMin\":%d,\"pressMax\":%d,\"pressOffset\":%d}",
+        min_temperatura, max_temperatura, offset_temperatura,
+        min_umidade, max_umidade, offset_umidade,
+        min_pressao, max_pressao, offset_pressao);
+
+    hs->len = snprintf(hs->response, sizeof(hs->response),
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/plain\r\n"
+        "Content-Length: %d\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        "%s",
+        json_len, json_payload);
+    }
+
+    else
+    {
+        hs->len = snprintf(hs->response, sizeof(hs->response),
+                           "HTTP/1.1 200 OK\r\n"
+                           "Content-Type: text/html\r\n"
+                           "Content-Length: %d\r\n"
+                           "Connection: close\r\n"
+                           "\r\n"
+                           "%s",
+                           (int)strlen(HTML_BODY), HTML_BODY);
+    }
 
 
     tcp_arg(tpcb, hs);
@@ -194,8 +377,14 @@ int main()
         return 1;
     }
     //wi-fi conectado com sucesso
-    printf("Falha ao conectar wi-fi");
+    printf("wi-fi conectado");
     wifi_connection = 1;
+
+     // Caso seja a interface de rede padrão - imprimir o IP do dispositivo.
+    if (netif_default)
+    {
+        printf("IP do dispositivo: %s\n", ipaddr_ntoa(&netif_default->ip_addr));
+    }
 
     //Inicializando sensores---------------------------------
 
@@ -215,14 +404,18 @@ int main()
 
     //Strings para mostrar valores no display
 
+    start_http_server();
+
     while (true) {
         
+        cyw43_arch_poll(); 
+
         //leitura do bmp280
         bmp280_read_raw(I2C_PORT_SENSOR, &temperatura_bmp, &pressao_bmp);
         int32_t convert_temperatura_bmp = bmp280_convert_temp(temperatura_bmp, &params);
-        pressao = bmp280_convert_pressure(pressao_bmp, temperatura_bmp, &params);
+        pressao = ((bmp280_convert_pressure(pressao_bmp, temperatura_bmp, &params))/1000) + offset_pressao;
 
-        printf("Pressao = %.3f kPa\n", pressao / 1000.0);
+        printf("Pressao = %.3f kPa\n", pressao);
         printf("Temperatura BMP: = %.2f C\n", convert_temperatura_bmp/ 100.0);
 
         //leitura do aht20
@@ -230,20 +423,20 @@ int main()
         {
             printf("Temperatura AHT: %.2f C\n", leituraAHT20.temperature);
             printf("Umidade: %.2f %%\n\n\n", leituraAHT20.humidity);
-            umidade = leituraAHT20.humidity;
+            umidade = leituraAHT20.humidity + offset_umidade;
         }
         else
         {
             printf("Erro na leitura do AHT10!\n\n\n");
         };
         //Calcula a média das temperaturas
-        temperatura_final = (convert_temperatura_bmp + leituraAHT20.temperature)/2;
+        temperatura_final = ((convert_temperatura_bmp/100 + leituraAHT20.temperature)/2) + offset_temperatura;
         //printf("Pressão : %d \n", convert_pressao_bmp);
         //função para o display
 
         //função para a matriz
         Niveis_Matriz(); //trste nivel umidade = 3, teste temperatura = 5
-        //display_dados (temperatura_final, 20, 40, 0, 45, 15); //colocar parâmetros depois --Usra variáveis globais
+        display_dados(); //colocar parâmetros depois --Usra variáveis globais
         sleep_ms(200);
     }
 };
@@ -311,7 +504,7 @@ void display_dados (){
 
     //strings para mostrar dados - 7 ao todo
     char str_pressao[5]; //para a pressão
-    int pressaok = pressao/1000;
+    int pressaok = pressao;
     sprintf(str_pressao,"%d",pressaok);
     char str_temp[5];
     sprintf(str_temp,"%.2f",temperatura_final);
